4b896436bdb56ad7b9ac6f2455e6a44bcd8ffd95
Vladimir Ulogov <33033499+vulogov-placeiq@users.noreply.github.com>
2017-11-08 13:32:45 -0500

    First working release

--- zabbix_agent.py
+++ zabbix_agent.py
@@ -1,6 +1,8 @@
 import socket
 import struct
 import sys
+import json
+import fnmatch
 
 def str2packed(data):
     header_field =  struct.pack('<4sBQ', 'ZBXD', 1, len(data))
@@ -31,5 +33,30 @@ def zabbix_get(**args):
     s.close()
     return response
 
+def get(host, key, var, _match, _second_key, op):
+    data =  json.loads(zabbix_get(host=host, port=10050, key=key))['data']
+    out = []
+    for d in data:
+	if d.has_key(var) and fnmatch.fnmatch(d[var], _match):
+	    out.append(float(zabbix_get(host=host, port=10050, key=_second_key.format(d[var]))))
+    if op == 'sum':
+	return '{0:.2f}'.format(sum(out))
+    elif op == 'avg':
+	return '{0:.2f}'.format(reduce(lambda x, y: x + y, out) / float(len(out)))
+    elif op == 'count':
+	return '{0:.2f}'.format(float(len(out)))
+    elif op == 'max':
+        return '{0:.2f}'.format(float(max(out)))
+    elif op == 'min':
+     	return '{0:.2f}'.format(float(min(out)))
+    elif op == 'delta':
+        return '{0:.2f}'.format(float(max(out)-min(out)))
+    return 0.0
+    
+
 if __name__ == '__main__':
-    print zabbix_get(host='phoenix-091.nym1.placeiq.net', port=10050, key='agent.version')
+    print get('phoenix-091.nym1.placeiq.net', 'vfs.fs.discovery', '{#FSNAME}', '/data/*', 'vfs.fs.size[{0},free]', 'sum')
+    print get('phoenix-091.nym1.placeiq.net', 'vfs.fs.discovery', '{#FSNAME}', '/data/*', 'vfs.fs.size[{0},pfree]', 'avg')
+    print get('phoenix-091.nym1.placeiq.net', 'vfs.fs.discovery', '{#FSNAME}', '/data/*', 'vfs.fs.size[{0},pfree]', 'min')
+    print get('phoenix-091.nym1.placeiq.net', 'vfs.fs.discovery', '{#FSNAME}', '/data/*', 'vfs.fs.size[{0},pfree]', 'max')
+    print get('phoenix-091.nym1.placeiq.net', 'vfs.fs.discovery', '{#FSNAME}', '/data/*', 'vfs.fs.size[{0},pfree]', 'delta')
